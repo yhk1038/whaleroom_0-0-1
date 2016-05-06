fce4a84f775bfa877184a4a9c56cfee4c6e3f102
SOHN DAI GEUN <nhos126@gmail.com>
2016-05-07 03:08:35 +0900

    160507 03:07 sdg Change to devise login view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
     # devise_for :users
 
-    devise_for :users, controllers: { sessions: "users/sessions" }
+    # devise_for :users, controllers: { sessions: "users/sessions" }
 
-    # devise_for :users, controllers: { registrations: 'users/registrations' }
+    devise_for :users, controllers: { registrations: 'users/registrations' }
     # devise_for :users, controllers: {
     #     registrations: 'users/registrations'
     # }
