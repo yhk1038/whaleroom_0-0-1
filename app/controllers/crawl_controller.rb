39cb8c88ac58ff2db7d7a1f6765798bac116ac96
yhk1038 <yhk1038@hanmail.net>
2016-05-06 15:48:59 +0900

    160506 15:46 yhk first yonghyun's local push

--- app/controllers/crawl_controller.rb
+++ app/controllers/crawl_controller.rb
@@ -63,8 +63,8 @@ class CrawlController < ApplicationController
     if Song.last == nil
       @start_num = 79999991   # 예제) 82425426 번은 악동뮤지션 200% 곡의 넘버임
     else
-      @start_num = 79999991
-      # @start_num = Song.last.song_num + 1
+      # @start_num = 79999991
+      @start_num = Song.last.song_num + 1
     end
     last_saved_song_count = Song.count
     
